b0d5301dd925723abda9f86c71836270d391ea2c
tanapol <junior_kid_za@hotmail.com>
2017-12-19 23:09:05 +0700

    add webhook

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const Client = require('@line/bot-sdk').Client;
 var app = express()
+
+const client = new Client({
+  channelAccessToken: 'vK2JgN/WLIb3Q/ZcIL2/2YDXCSi74w8XIz+Tmd7JJu5KXOtVTBjOxo8d/LHlHFA7EhuDeXgJOUoE6rEUNZCWlX09Z7IXBIFECNvw5KPVo5/gUYKq6vkaPp387RT2wm5G0AWIc7AAlta5B5pe6gJSqAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'f787f2e45e6e2dcaaa8068d5d905181f'
+});
+
+// client.pushMessage(userId, { type: 'text', text: 'hello, world' })
+
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+
+  const message = {
+    type: 'text',
+    text: 'Hello World!'
+  };
+  
+  client.replyMessage(replyToken, message)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch((err) => {
+      res.sendStatus(400)
+    });
+})
 
 app.use(bodyParser.json())
 
